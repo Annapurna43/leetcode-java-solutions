ecdad9155254152fcdfa937d776220c13e0a1644
Annapurna Mahato <84138282+Annapurna43@users.noreply.github.com>
2026-06-25 20:44:56 +0100

    Time: 0 ms (100%), Space: 42.1 MB (53.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,8 +1,8 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n<=1){
-            return n;
-        }
+        //if(n<=1){
+          //  return n;
+        //}
         int[] dp = new int[n+1];
         dp[0] = 1;
         dp[1] = 1;
