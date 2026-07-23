0803e74d667c89b3e85bb6a4d030f70e5d599c1f
Annapurna Mahato <84138282+Annapurna43@users.noreply.github.com>
2026-07-23 17:10:14 +0100

    Time: 33 ms (20.45%), Space: 47.4 MB (49.69%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minInsertions(String s) {
+      int len = s.length();
+      return len - longestPalindromeSubseq(s);  
+        
+    }
+    public int longestPalindromeSubseq(String s) {
+        String text1 = s;
+        String text2 = new StringBuilder(s).reverse().toString();
+        int dp[][] = new int[text1.length()][text2.length()];
+        for(int row[]: dp){
+            Arrays.fill(row, -1);
+        }
+        return solve(text1.length()-1, text2.length()-1,text1, text2,dp);
+    }
+
+    int solve(int i, int j, String s1, String s2, int dp[][]){
+        if(i < 0 || j < 0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s1.charAt(i) == s2.charAt(j)) return 1 + solve(i-1, j-1, s1, s2, dp);
+
+        return dp[i][j] = Math.max(solve(i-1, j, s1, s2, dp), solve(i, j-1, s1, s2, dp));
+    }
+}
